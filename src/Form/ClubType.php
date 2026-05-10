5fdba7778ce3b7a926927770cb3694aad585d1e0
zakaria <zakariahaj326@gmail.com>
2026-05-10 13:55:19 +0100

    creation du club avec upload d'image logo du club

---
+++ src/Form/ClubType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Club;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ClubType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('domain')
+            ->add('logoFile', FileType::class, [
+                'label' => 'Club Logo (image)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/webp', 'image/gif'],
+                        'mimeTypesMessage' => 'Please upload a valid image (JPG, PNG, WEBP or GIF)',
+                    ]),
+                ],
+            ])
+            ->add('wbsite')
+            ->add('status')
+            ->add('createdAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Club::class,
+        ]);
+    }
+}
